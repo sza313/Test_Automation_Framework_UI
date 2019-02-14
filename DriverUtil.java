6792dae8e99ce69518ea9131df6c03a68535200d
annanagyroczei <36069072+annanagyroczei@users.noreply.github.com>
2019-02-14 16:08:28 +0100

    Added methods
    Added methods: autoSuggestionField, clearDataFromField, dragAndDropWebelement, waitingForElementToBeVisible

--- DriverUtil.java
+++ DriverUtil.java
@@ -5,8 +5,11 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.io.FileInputStream;
 import java.io.InputStream;
@@ -161,5 +164,98 @@ public class DriverUtil {
         }
         return true;
     }
+    /** clickButton method is clicking on a certain button on the apage
+     * Params:
+     * WebElement button: unique ID or path to the button
+     */
+    public boolean clickButton(WebElement button){
+        try {
+            Log.info("Writing alphanumeric string into the following textbox. ID=" + button.getAttribute("id") + " , CLASS=" + button.getAttribute("class")  + " , NAME=" + button.getAttribute("name"));
+            button.click();
+        }
+        catch (NoSuchElementException e){
+            Log.error("Could not find the requested textbox.");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+    /** autoSuggestionField method is writing a text into an
+     * autosuggestive field and selects the first result
+     * Params:
+     * WebElement textBox: unique ID or path to the textbox
+     * String text: text entered to which we want to find autosuggestion
+     */
+    public boolean autoSuggestionField(WebElement textBox, String text) {
+            try {
+                Log.info("Writing text " + text + " into the following textbox and selecting auto suggestion. ID=" + textBox.getAttribute("id") + " , CLASS=" + textBox.getAttribute("class")  + " , NAME=" + textBox.getAttribute("name"));
+                textBox.sendKeys(text);
+                textBox.sendKeys(Keys.ENTER);
+            }
+            catch (NoSuchElementException e){
+                Log.error("Could not find the requested textbox.");
+                e.printStackTrace();
+                return false;
+            }
+            return true;
+    }
+    /** clearDataFromField method is deleting the data from a field
+     * and then checks for the length of the field to make sure it is empty
+     * Params:
+     * WebElement textBox: unique ID or path to the textbox
+     */
+    public boolean clearDataFromField(WebElement textBox) {
+        try {
+            Log.info("Clearing data from the following textbox. ID=" + textBox.getAttribute("id") + " , CLASS=" + textBox.getAttribute("class")  + " , NAME=" + textBox.getAttribute("name"));
+            textBox.clear();
+            textBox.getAttribute("value");
+        }
+        catch (NoSuchElementException e){
+            Log.error("Could not find the requested textbox.");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 
+    /** clearDataFromField method is deleting the data from a field
+     * and then checks for the length of the field to make sure it is empty
+     * Params:
+     * WebElement draggable: unique ID or path to the element we want to drag
+     * WebElement droppable: unique ID or path to the element in which we want to drop the draggable element
+     */
+    public boolean dragAndDropWebelement(WebElement draggable, WebElement droppable) {
+        try {
+            Log.info("Dragging and dropping the following element: ID=" + draggable.getAttribute("id") + " , CLASS=" + draggable.getAttribute("class")  + " , NAME=" + draggable.getAttribute("name") + " into element ID= " + droppable.getAttribute("id") + " , CLASS=" + draggable.getAttribute("class")  + " , NAME=" + draggable.getAttribute("name"));
+            Actions action = new Actions(driver);
+            WebElement source = draggable;
+            WebElement target = droppable;
+            action.dragAndDrop(source, target).build().perform();
+        }
+        catch (NoSuchElementException e){
+            Log.error("Could not find the requested elements.");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    /** clearDataFromField method is deleting the data from a field
+     * and then checks for the length of the field to make sure it is empty
+     * Params:
+     * WebElement textBox: unique ID or path to the textbox
+     */
+    public boolean waitingforElementToBeVisible(WebElement element, int seconds) {
+        try {
+            Log.info("Waiting for the following element to be visible: ID=" + element.getAttribute("id") + " , CLASS=" + element.getAttribute("class")  + " , NAME=" + element.getAttribute("name"));
+            WebDriverWait wait = new WebDriverWait(driver, seconds);
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("element")));
+        }
+        catch (NoSuchElementException e){
+            Log.error("Could not find the requested element.");
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
 }
