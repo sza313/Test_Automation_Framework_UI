133e1a128b3a6f6c481751890d5744560a061718
Gyula Szecsi <gyulawww@hotmail.com>
2019-03-20 09:34:31 +0100

    Add missing import to DriverUtil.java

--- src/main/java/selenium/utils/DriverUtil.java
+++ src/main/java/selenium/utils/DriverUtil.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
